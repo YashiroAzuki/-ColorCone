05dbed884a99c254cb14a6b4dcbded97168d8742
19jy0109 <19jy0109@PC11.jynet2.jec.ac.jp>
2020-11-05 14:35:20 +0900

    Soft

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 		System.out.println("HelloWorld");
 		System.out.println("Java");
 		System.out.println("HTML5");
+		System.out.println("SQL");
+		System.out.println("Soft");
 	}
 }
